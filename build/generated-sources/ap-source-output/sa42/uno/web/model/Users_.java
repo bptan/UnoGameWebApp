546f5a812ad180f00d4dab3b3b957f7b00085056
bptan <t.boonpiao@gmail.com>
2016-08-17 18:03:39 +0800

    edit index

--- build/generated-sources/ap-source-output/sa42/uno/web/model/Users_.java
+++ build/generated-sources/ap-source-output/sa42/uno/web/model/Users_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-17T15:14:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-17T16:48:09")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
